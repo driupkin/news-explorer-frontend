4f48f37d711d12d0e6224149cc80cbbf7741dc91
Stas Borisov <driupkin@mail.ru>
2020-11-16 03:45:30 +0300

    seved news mod

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,10 +7,17 @@ function Header(props) {
         <header className={`header ${props.isSevedNews ? 'header_theme_white' : ''}`}>
             <div className={`header__logo ${props.isSevedNews ? 'header__logo_theme_white' : ''}`}></div>
             <Navigation>
-                <a href="/" className={`header__link ${props.isSevedNews ? 'header__link_theme_white' : ''}`}>Главная</a>
+                <a
+                    href="/"
+                    className={`header__link ${props.isSevedNews ? 'header__link_theme_white' : 'header__link_undeline'}`}>Главная</a>
                 <a
                     href="/saved-news"
-                    className={`header__link ${props.isSevedNews ? 'header__link_theme_white' : ''}`}>Сохранённые статьи
+                    className={
+                        `header__link 
+                        ${props.isSevedNews
+                            ? 'header__link_theme_white header__link_undeline'
+                            : ''}`
+                    }>Сохранённые статьи
                 </a>
                 <button
                     className={`header__button ${props.isSevedNews ? 'header__button_theme_white' : ''}`}
